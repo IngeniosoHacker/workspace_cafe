726a6bb7a8d848f819fadcb62b21394caa4cf34b
BlueBarrier <dannbj1@gmail.com>
2025-10-17 08:40:05 -0600

    SQL config esquemas

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Workspace Cafe
 
----
+Proyecto dónde se crea una estr simuctura simple de bases de datos con sql para controlar una cafetería enfocada al coworking. 
---
+++ sql/esquema.sql
@@ -0,0 +1,77 @@
+CREATE TABLE IF NOT EXISTS menu(
+	id serial PRIMARY KEY,
+	descripcion varchar(200),
+	precio numeric(10,2),
+	tipo varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS sede(
+	id serial PRIMARY KEY,
+	direccion varchar(120) not null
+);
+
+CREATE TABLE IF NOT EXISTS inventario(
+	id serial PRIMARY KEY,
+	producto varchar(50),
+	existencia int not null,
+	caducidad date not null,
+	sede_id int REFERENCES sede(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS receta(
+	menuid int REFERENCES menu(id),
+	inventid int REFERENCES inventario(id)
+);
+
+CREATE TABLE IF NOT EXISTS cliente(
+	id serial PRIMARY KEY,
+	nombre varchar(120) not null,
+	telefono bigint,
+	platoFav int,
+	suscripcion varchar(50),
+	FOREIGN KEY (platoFav) REFERENCES menu(id)
+);
+
+CREATE TABLE IF NOT EXISTS espacio(
+	id serial PRIMARY KEY,
+	tipo varchar(50) not null,
+	precio numeric(10,2)
+);
+
+
+CREATE TABLE IF NOT EXISTS personal(
+	id serial PRIMARY KEY,
+	nombre varchar(120) not null,
+	puesto varchar(50) not null,
+	password varchar(20) not null,
+	salary numeric(10,2),
+	sede_id int REFERENCES sede(id)
+);
+
+CREATE TABLE IF NOT EXISTS reserva(
+	id serial PRIMARY KEY,
+	horaFecha timestamp DEFAULT NOW(),
+	numPersonas int not null,
+	status varchar(20),
+	espacio_fk int REFERENCES espacio(id),
+	sede_fk int REFERENCES sede(id),
+	cliente_fk int REFERENCES cliente(id),
+	personal_fk int REFERENCES personal(id)
+);
+
+CREATE TABLE IF NOT EXISTS pide(
+	reservaid int REFERENCES reserva(id),
+	menuid int REFERENCES menu(id)
+);
+
+CREATE TABLE IF NOT EXISTS bitacora(
+	id serial PRIMARY KEY,
+	esquema varchar(50) not null,
+	id_afectado int not null,
+	operacion varchar(50),
+	fecha timestamp DEFAULT NOW(),
+	detalle varchar(120),
+	personal_id int not null
+);
+
